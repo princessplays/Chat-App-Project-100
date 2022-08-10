b0ca453786555bbb16320d6b435d1407ed423627
princessplays <102701250+princessplays@users.noreply.github.com>
2022-08-09 20:08:09 -0400

    Add files via upload

--- chat.js
+++ chat.js
@@ -15,6 +15,22 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+
+  function addRoom()
+{
+      Room_names = document.getElementById("Room_names").value;
+
+      firebase.database().ref("/").child(Room_names).update({
+        purpose: "adding room name"      
+      
+      });
+
+      localStorage.setItem("Room_names", Room_names );
+      window.location = "kwitter_page.html";
+
+}
+
+
 function addUser()
 {
     user_name = document.getElementById("user_name").value;
@@ -24,6 +40,16 @@ function addUser()
     localStorage.setItem("user_name", user_name);
     window.location = "chat_room.html";
 }
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+       console.log("Room Name -" + Room_names);
+       row = "<div class='Rooms_names' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+ Room_names + "</div><hr>";
+       document.getElementById("output").innerHTML += row;
+       //End code
+      });});}
+getData();
+
 
 
 
